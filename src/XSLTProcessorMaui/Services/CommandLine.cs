00bebfacdf5596554b551bc24cde70624b32d812
Ikosy <lendres@fifthrace.com>
2025-02-02 07:40:21 -0600

    Create argument classes

---
+++ src/XSLTProcessorMaui/Services/CommandLine.cs
@@ -0,0 +1,70 @@
+﻿using DigitalProduction.CommandLine;
+
+namespace XSLTProcessorMaui;
+
+[CommandLineManager(ApplicationName = "XSLT Transformer", Copyright = "Copyright (c) Lance A. Endres.")]
+internal class CommandLine
+{
+	#region Properties
+
+	/// <summary>
+	/// The XML file the is the source material.
+	/// </summary>
+	[CommandLineOption(Name = "inputfile", Description = "XML file that is the source material.")]
+	public string InputFile { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The transformation sheet.
+	/// </summary>
+	[CommandLineOption(Name = "xsltfile", Description = "XSLT file.")]
+	public string XsltFile { get; set; } = string.Empty;
+
+	/// <summary>
+	/// The transformation sheet.
+	/// </summary>
+	[CommandLineOption(Name = "xsltargs", Description = "XSLT arguments.")]
+	public string XsltArguments { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Output (destination) file.
+	/// </summary>
+	[CommandLineOption(Name = "outputfile", Description = "Where the output is written to.")]
+	public string OutputFile { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Output (destination) file.
+	/// </summary>
+	[CommandLineOption(Name = "runpostprocessor", BoolFunction = BoolFunction.TrueIfPresent, Description = "Specifies if the post processor should be run.")]
+	public bool RunPostProcessor { get; set; } = false;
+
+	/// <summary>
+	/// Output (destination) file.
+	/// </summary>
+	[CommandLineOption(Name = "postprocessor", Description = "Post processing routine to run after XSLT transformation is completed.")]
+	public string PostProcessor { get; set; } = string.Empty;
+
+	#endregion
+
+	#region
+
+	/// <summary>
+	/// Parse the command line arguments.
+	/// </summary>
+	public void ParseCommandLine()
+	{
+		// Create the parser to handle the command line arguments.  See the properties to determine valid command line arguments.
+		CommandLineParser parser = new(this);
+
+		// We need to replace the existing quotation information because if we don't it replaces network address such as "\\computer" 
+		// with "\computer" because it escapes the escape character.
+		QuotationInfo quotationInfo = new('\"');
+		quotationInfo.AddEscapeCode('\"', '\"');
+		parser.AddQuotation(quotationInfo);
+
+		// Parse the command line arguments.  The parser will call Properties for each command line argument it finds with a matching
+		// attribute.
+		parser.Parse();
+	}
+
+	#endregion
+}
---
+++ src/XSLTProcessorMaui/Services/ICommandLine.cs
@@ -0,0 +1,40 @@
+﻿namespace XSLTProcessorMaui;
+
+internal interface ICommandLine
+{
+	/// <summary>
+	/// The XML file the is the source material.
+	/// </summary>
+	public string InputFile { get; set; }
+
+	/// <summary>
+	/// The transformation sheet.
+	/// </summary>
+	public string XsltFile { get; set; }
+
+	/// <summary>
+	/// The transformation sheet.
+	/// </summary>
+	public string XsltArguments { get; set; }
+
+	/// <summary>
+	/// Output (destination) file.
+	/// </summary>
+	public string OutputFile { get; set; }
+
+	/// <summary>
+	/// Output (destination) file.
+	/// </summary>
+	public bool RunPostProcessor { get; set; }
+
+	/// <summary>
+	/// Output (destination) file.
+	/// </summary>
+	public string PostProcessor { get; set; }
+
+
+	/// <summary>
+	/// Parse the command line arguments.
+	/// </summary>
+	public void ParseCommandLine();
+}
